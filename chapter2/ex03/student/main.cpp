5418e514ce735bf09df8c6b9cbeace4254c79cba
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-10-29 04:39:48 +0000

    Testing Tue Oct 29 2024 04:39:48 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex03/student/main.cpp
+++ chapter2/ex03/student/main.cpp
@@ -11,7 +11,7 @@ int num3 = -25;
 int average = 0;
 //executable statements
 average = (num1 + num2 + num3 ) / 3 ;
-cout<< "Average of three num is: " << average << endl ;
+cout<<"Average of three num is: "<<average<<endl;
 //return 0;
 return 0;
 }

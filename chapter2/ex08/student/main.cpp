9966ed3c9991e2e878aa1d771716a7614978fc00
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-11-02 02:59:56 +0000

    Testing Sat Nov 02 2024 02:59:56 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex08/student/main.cpp
+++ chapter2/ex08/student/main.cpp
@@ -1,13 +1,37 @@
 //include statement(s)
-
+#include<iostream>
+#include<string>
 //using namespace statement
-
+using namespace std;
 int main()
 {
 
     //variable declaration
-    
+    const int SECRET = 11;
+    double RATE = 12.50;
+    int num1;
+    int num2;
+    int num3;
+    int newNum;
+    string name;
+    double hoursWorked;
+    double wages;
     //executable statements
-    
+    cout<<"Enter two integer: "<<endl;
+    cin>>num1>>num2;
+    cout<<"Enter a person's last name: "<<endl;
+    cin>>name;
+    cout<<"Enter a number of hours worked between 0 to 70 : "<<endl;
+    cin>>hoursWorked;
+    wages = RATE * hoursWorked;
+    cout<<"The value of num1 = "<<num1<<" and the value of num2 is "<<num2<<"."<<endl;
+    newNum = num1 * 2 + num2 ;
+    cout<<"The value of newNumv = "<< newNum<<endl;
+    newNum = newNum + SECRET;
+    cout<<"The value of newNum = "<<newNum<<endl;
+
+    cout<<"Name: "<<name<<"\nPay Rate: $"<<RATE<<"\nHours Worked: "<<hoursWorked<<"\nSalary: $"<<wages<<endl;
+
+
     //return statement
 }

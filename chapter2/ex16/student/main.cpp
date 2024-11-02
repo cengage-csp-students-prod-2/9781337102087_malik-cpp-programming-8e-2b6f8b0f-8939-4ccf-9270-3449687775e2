a7afb22f4ba909d495e1aef77c850467a2da0588
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-11-02 13:38:39 +0000

    Testing Sat Nov 02 2024 13:38:39 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex16/student/main.cpp
+++ chapter2/ex16/student/main.cpp
@@ -10,7 +10,7 @@ int main() {
     double amount;
     cout<<"Enter the total amount of milk produced in the morning"<<endl;
     cin>>amount;
-    int newCartons = static_cast<int>(amount / milk_rate););
+    int newCartons = static_cast<int>(amount / milk_rate);
     cout<<"The number of milk cartoons needed to hold the milk = "<< newCartons <<endl;
     cout<<"THe cost of producing the milk = "<< milk_one_litre *amount<<endl;
     cout<<"The profit for producing the milk = "<< profit_carton * newCartons<<endl;

1bf85ce16d115493337638f2693272b10ebfdee0
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-11-03 22:57:13 +0000

    Testing Sun Nov 03 2024 22:57:13 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex04/student/main.cpp
+++ chapter3/ex04/student/main.cpp
@@ -1,6 +1,7 @@
 //Logic errors.
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 
 using namespace std;
 
@@ -19,11 +20,11 @@ int main()
     cin >> cost;
 
     
-    cout << "Enter the area, in square feet, that can be fertilized by one bag: ";
+    cout << "Enter the area in square feet that can be fertilized by one bag: ";
     cin >> area;
     
-    cout << "The cost of the fertilizer per pound is: $" << fixed << showpoint << setprecision(2)<< bagSize / cost << endl;
+    cout << "The cost of the fertilizer per pound is: $" << fixed << showpoint << setprecision(2)<< round(bagSize / cost )<< endl;
     
-    cout << "The cost of fertilizing per square foot is: $"  << fixed << showpoint << setprecision(2)<<area / cost << endl;
+    cout << "The cost of fertilizing per square foot is: $"  << fixed << showpoint << setprecision(2)<<round(area / cost) << endl;
     return 0;
 }

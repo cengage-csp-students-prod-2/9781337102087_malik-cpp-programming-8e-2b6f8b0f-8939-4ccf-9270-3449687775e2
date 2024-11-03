d7ad1a8d13a6c9e2a8f072e5de8e04f0b58dacc0
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-11-03 22:17:08 +0000

    Testing Sun Nov 03 2024 22:17:08 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex16/student/main.cpp
+++ chapter2/ex16/student/main.cpp
@@ -11,10 +11,11 @@ int main() {
     cout<<"Enter the total amount of milk produced in the morning"<<endl;
     cin>>total_milk;
     double newCartons = ceil(total_milk / milk_rate);
-    
+    double production_cost = milk_one_litre * total_milk;
+    double productin_profit = newCartons * profit_carton;
     cout<<"The number of milk cartoons needed to hold the milk = "<< newCartons <<endl;
-    cout<<"THe cost of producing the milk = "<< milk_one_litre * total_milk<<endl;
-    cout<<"The profit for producing the milk = "<< newCartons * profit_carton <<endl;
+    cout<<"THe cost of producing the milk = "<< production_cost<<endl;
+    cout<<"The profit for producing the milk = "<< productin_profit <<endl;
 
 
     return 0;

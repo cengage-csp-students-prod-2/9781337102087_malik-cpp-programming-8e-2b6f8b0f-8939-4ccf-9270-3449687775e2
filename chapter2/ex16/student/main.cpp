944770c7cac5d5e521ddee581b58dcfff0dc6c74
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-11-03 22:22:03 +0000

    Testing Sun Nov 03 2024 22:22:03 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex16/student/main.cpp
+++ chapter2/ex16/student/main.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
-#include<cmath> 
+#include <cmath>    // For the ceil function
+#include <iomanip>  // For setting decimal precision
+
 using namespace std;
 
 int main() {
-    // Write your main here
-    double milk_rate = 3.78;
-    double milk_one_litre = 0.38;
-    double profit_carton = 0.27;
-    double total_milk;
-    cout<<"Enter the total amount of milk produced in the morning"<<endl;
-    cin>>total_milk;
-    double newCartons = ceil(total_milk / milk_rate);
-    double production_cost = milk_one_litre * total_milk;
-    double productin_profit = newCartons * profit_carton;
-    cout<<"The number of milk cartoons needed to hold the milk = "<< newCartons <<endl;
-    cout<<"THe cost of producing the milk = "<< production_cost<<endl;
-    cout<<"The profit for producing the milk = "<< productin_profit <<endl;
+    double milk_rate = 3.78;           // Capacity of each milk carton in liters
+    double milk_one_litre = 0.38;      // Cost of producing one liter of milk
+    double profit_carton = 0.27;       // Profit per carton of milk
+    double total_milk;                 // Total milk produced in liters
 
+    cout << "Enter the total amount of milk produced in the morning: ";
+    cin >> total_milk;
+
+    // Calculations
+    int cartons_needed = ceil(total_milk / milk_rate);  // Number of cartons needed, rounded up
+    double production_cost = milk_one_litre * total_milk;
+    double production_profit = cartons_needed * profit_carton;
+
+    // Output results with formatting
+    cout << "The number of milk cartons needed to hold the milk = " << cartons_needed << endl;
+    cout << "The cost of producing the milk = " << fixed << setprecision(2) << production_cost << endl;
+    cout << "The profit for producing the milk = " << fixed << setprecision(2) << production_profit << endl;
 
     return 0;
-
 }

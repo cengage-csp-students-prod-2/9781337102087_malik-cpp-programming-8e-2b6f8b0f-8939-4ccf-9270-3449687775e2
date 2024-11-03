fe377fe2426c7be13ecac963fdda0e18d51071eb
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-11-03 22:25:46 +0000

    Testing Sun Nov 03 2024 22:25:46 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex16/student/main.cpp
+++ chapter2/ex16/student/main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-    double milk_rate = 3.78;           // Capacity of each milk carton in liters
+    double milk_per_carton = 3.78;           // Capacity of each milk carton in liters
     double milk_one_litre = 0.38;      // Cost of producing one liter of milk
     double profit_carton = 0.27;       // Profit per carton of milk
     double total_milk;                 // Total milk produced in liters
@@ -14,12 +14,12 @@ int main() {
     cin >> total_milk;
 
     // Calculations
-    int cartons_needed = ceil(total_milk / milk_rate);  // Number of cartons needed, rounded up
+    int cartons_needed = ceil(total_milk / milk_per_carton);  // Number of cartons needed, rounded up
     double production_cost = milk_one_litre * total_milk;
     double production_profit = cartons_needed * profit_carton;
 
     // Output results with formatting
-    cout << "The number of milk cartons needed to hold the milk = " << cartons_needed << endl;
+    cout << "The number of milk cartons needed to hold the milk = " << total_milk/milk_one_litre<< endl;
     cout << "The cost of producing the milk = " << fixed << setprecision(2) << production_cost << endl;
     cout << "The profit for producing the milk = " << fixed << setprecision(2) << production_profit << endl;
 

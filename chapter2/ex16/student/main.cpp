ba7f216234487eccc0388447c14e7d10e1345912
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-11-03 22:13:47 +0000

    Testing Sun Nov 03 2024 22:13:47 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex16/student/main.cpp
+++ chapter2/ex16/student/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include<cmath> 
 using namespace std;
 
 int main() {
@@ -7,14 +7,14 @@ int main() {
     double milk_rate = 3.78;
     double milk_one_litre = 0.38;
     double profit_carton = 0.27;
-    double amount;
+    double total_milk;
     cout<<"Enter the total amount of milk produced in the morning"<<endl;
-    cin>>amount;
-    int newCartons = (amount / milk_rate);
-
+    cin>>total_milk;
+    double newCartons = ceil(total_milk / milk_rate);
+    
     cout<<"The number of milk cartoons needed to hold the milk = "<< newCartons <<endl;
-    cout<<"THe cost of producing the milk = "<< milk_one_litre *amount<<endl;
-    cout<<"The profit for producing the milk = "<< newCartons<<endl;
+    cout<<"THe cost of producing the milk = "<< milk_one_litre * total_milk<<endl;
+    cout<<"The profit for producing the milk = "<< newCartons * profit_carton <<endl;
 
 
     return 0;

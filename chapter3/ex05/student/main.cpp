47c179c5df2b05419367a4a685072118e8b6c7e3
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-11-09 20:35:04 +0000

    Testing Sat Nov 09 2024 20:35:04 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex05/student/main.cpp
+++ chapter3/ex05/student/main.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
-
+#include <fstream>
+#include<ios>
+#include<string>
+#include <iomanip>
+// Declared header files
 using namespace std;
 
 int main() {
+// Declaring file objects and local variables
+    fstream inFile; //for. input file
+    fstream outFile; // for output file
+
+    string firstName; // for first name
+    string lastName; //for lastname
+    string fileName; // for fileName for output
+    int salary;
+    int increasedRate;
+    int updatedSalary;
+
+    // Request the filename for output
+    // cout<<"Enter the file name for output: "<< endl;
+    // cin>>fileName;
+
+    inFile.open("Ch3_Ex5Data.txt");
+    outFile.open("Ch3_Ex5Output.dat");
+    //set the precision for the output file
+    outFile<<fixed<<showpoint<<setprecision(2);
+    if(inFile.is_open()){
+        while (inFile.good() ){
+        // read the data from file 
+            
+            inFile>>lastName;
+            inFile>>firstName;
+            inFile>>salary;
+            inFile>> increasedRate;
+
+            // calculating Updating salary
+
+            updatedSalary = salary *( 1 + increasedRate / 100 );
+
+            // Writibg the data to the outputfile
+
+            outFile<< firstName;
+            outFile<< lastName ;
+            outFile<< updatedSalary ;
+        
+
+        }
+    }
+    inFile.close();
+    outFile.close();
+
+
+
+
+
     // Write your main here
     return 0;
 }

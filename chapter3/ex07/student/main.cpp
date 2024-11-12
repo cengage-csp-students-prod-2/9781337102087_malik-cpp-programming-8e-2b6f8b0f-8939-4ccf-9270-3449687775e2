088f23582c38478e90bf2c7811d02a71d29452f3
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-11-12 18:20:33 +0000

    Testing Tue Nov 12 2024 18:20:33 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex07/student/main.cpp
+++ chapter3/ex07/student/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main() {
     // Write your main here
+    double averageDailyBalance;
+    double netBalance;
+    double d1;
+    double payment;
+    double d2;
+    double interest;
+    double interestRate;
+
+    cout<<fixed<<setprecision(2);
+    cout<<"Enter netBalance"<<endl;
+    cin>>netBalance;
+    cout<<"Enter d1"<<endl;
+    cin>>d1;
+    cout<<"Enter payment"<<endl;
+    cin>>payment;
+    cout<<"Enter d2"<<endl;
+    cin>>d2;
+    cout<<"Enter interestRate"<<endl;
+    cin>>interestRate;
+    
+    averageDailyBalance = (netBalance * d1 - payment * d2) / d1;
+    interest = averageDailyBalance * interestRate;
+    cout<<averageDailyBalance;
+
     return 0;
 }

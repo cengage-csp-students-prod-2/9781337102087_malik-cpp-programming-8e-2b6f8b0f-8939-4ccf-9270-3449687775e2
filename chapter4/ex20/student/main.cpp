7f906c544bdd0c67bd307bf5e1d13bda9a8171fc
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-11-13 11:05:49 +0000

    Testing Wed Nov 13 2024 11:05:49 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex20/student/main.cpp
+++ chapter4/ex20/student/main.cpp
@@ -40,29 +40,30 @@ int main() {
     // output
     cout<<"The cost of renting one room are: "<<cost_of_room<<endl;
     if (no_of_rooms >=10 && no_of_rooms < 20 )
-        discount = cost_of_room * ( TEN_TO_NINTEEN_ROOMS_DIS /100);
+        discount =  ( TEN_TO_NINTEEN_ROOMS_DIS );
     else if( no_of_rooms >=20 && no_of_rooms<30)
-        discount = cost_of_room*( TWENTY_TO_TWENTYNINE_ROOMS_DIS/100);
+        discount = TWENTY_TO_TWENTYNINE_ROOMS_DIS;
     else if( no_of_rooms >=30)
-        discount = cost_of_room*(THIRTY_OR_MORE_ROOMS_DISC/100);
+        discount = THIRTY_OR_MORE_ROOMS_DISC;
     else{
         discount = 0;
         cout<<"No discount availiable according to no of rooms booked."<<endl;
     }
     if (no_of_days>=3)
-        discount_no_of_room = cost_of_room*(THREE_DAYS_ROOM_DISC/100);
+        discount_no_of_room = THREE_DAYS_ROOM_DISC;
     else
         discount_no_of_room = 0;
 
-    discount_on_each_room = ((discount+discount_no_of_room)/no_of_rooms);
+   
 
-    discount_in_percent = discount_on_each_room * 100/ cost_of_room;
+    discount_in_percent = ((discount+discount_no_of_room));
+    discount_on_each_room = cost_of_room *(1-discount_in_percent/100);
 
     cout<<"The discount on each room as a percent is "<<discount_in_percent<<endl;
     cout<<"The number of rooms booked are "<<no_of_rooms<<endl;
-    cout<<"the number of days the rooms booked are "<<no_of_rooms<<endl;
+    cout<<"the number of days the rooms booked are "<<no_of_days<<endl;
 
-    total_cost = (cost_of_room - discount_on_each_room) * no_of_days * no_of_rooms;
+    total_cost = discount_on_each_room* no_of_days * no_of_rooms;
 
 
     cout<<"The total cost of the rooms "<< total_cost<<endl;
@@ -71,15 +72,6 @@ int main() {
 
     billing_amount = total_cost*(1+ sales_tax/100);
     cout<<"The total billing amount is "<<billing_amount<<endl;
-
-
-    
-    
-    
-    
-
-
-
 
     return 0;
 }

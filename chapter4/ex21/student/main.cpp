3b2a22281cf9e0b8ac064090f77853002650a43c
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-11-13 13:15:24 +0000

    Testing Wed Nov 13 2024 13:15:24 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex21/student/main.cpp
+++ chapter4/ex21/student/main.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main() {
     // Write your main here
-    int x1;
-    int y1;
-    int x2;
-    int y2;
-    int m;
-    int b;
+    double x1;
+    double y1;
+    double x2;
+    double y2;
+    double m;
+    double b;
 
     cout<<"Input Input x1, Input y1, Input x2, Input y2"<<endl;
     cin>>x1>>y1>>x2>>y2;
@@ -19,7 +19,7 @@ int main() {
     cout << "The line passing through the points (" << x1 << ", " << y1<< ") and (" << x2 << ", " << y2 << ") is x = " << x1 << " and it is a vertical line." << endl;
     else {
     m = (y2 - y1) / (x2 - x1);
-    b = -m * x1 + y1;
+    b = -m * (x1 + y1);
     cout << "The line passing through the points (" << x1 << ", " << y1<< ") and (" << x2 << ", " << y2 << ") is y = "<< m << "x + " << b << endl;
     }
     if (m == 0)

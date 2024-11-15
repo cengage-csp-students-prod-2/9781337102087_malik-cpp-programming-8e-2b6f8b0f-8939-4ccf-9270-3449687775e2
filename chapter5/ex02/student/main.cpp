32c4c1e42ccb0d395e4493a9908cc22d6f707a82
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-11-15 10:22:43 +0000

    Testing Fri Nov 15 2024 10:22:43 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex02/student/main.cpp
+++ chapter5/ex02/student/main.cpp
@@ -6,20 +6,23 @@ using namespace std;
 int main()
 {
     double pi = 0;
-    long i;
-    long n;
+    int i;
+    int n;
     cout << "Enter the value of n: "<<endl;
     cin >> n;
     
     
      for (i = 0; i < n; i++)
     {
-        if (i % 2 == 0)
-            pi = pi + (1 / (2 * i + 1));
-        else
-            pi = pi - (1 / (2 * i + 1));
+        if (i % 2 == 0){
+            pi = pi + (1.0 / (2 * i + 1));
+        }
+        else{
+            pi = pi - (1.0 / (2 * i + 1));
     
-    }
+    }}
+
+    cout<<fixed<<showpoint<<setprecision(15);
     pi = 4 * pi;
     
     

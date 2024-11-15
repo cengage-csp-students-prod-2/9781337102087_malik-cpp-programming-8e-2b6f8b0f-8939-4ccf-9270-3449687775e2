3326fdf234001a44f29827ec3da10df148588a8e
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-11-15 10:11:36 +0000

    Testing Fri Nov 15 2024 10:11:36 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex02/student/main.cpp
+++ chapter5/ex02/student/main.cpp
@@ -8,22 +8,21 @@ int main()
     double pi = 0;
     long i;
     long n;
-    
+    cout << "Enter the value of n: "<<endl;
     cin >> n;
-    cout << "Enter the value of n: ";
-    cout << endl;
     
-    if (i % 2 == 0)
-        pi = pi + (1 / (2 * i + 1));
-    else
-        pi = pi - (1 / (2 * i + 1));
     
-    for (i = 0; i < n; i++)
+     for (i = 0; i < n; i++)
     {
-        pi = 0;
-        pi = 4 * pi;
-    }
+        if (i % 2 == 0)
+            pi = pi + (1 / (2 * i + 1));
+        else
+            pi = pi - (1 / (2 * i + 1));
     
-    cout << endl << "pi = " << pi << endl;
+    }
+    pi = 4 * pi;
+    
+    
+    cout << "pi = " << pi << endl;
     return 0;
 }

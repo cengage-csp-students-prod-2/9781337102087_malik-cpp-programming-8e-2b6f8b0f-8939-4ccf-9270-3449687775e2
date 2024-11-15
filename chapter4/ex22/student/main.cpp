2cdfe43fc5a5ff475856f069e38c692de6af0cce
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-11-15 09:56:18 +0000

    Testing Fri Nov 15 2024 09:56:18 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex22/student/main.cpp
+++ chapter4/ex22/student/main.cpp
@@ -10,15 +10,15 @@ int main() {
     cout<<"Enter a num betwen 1-1000: "<<endl;
     cin>>num;
     int c = 0;
-    for(int i=0;i<11;i++){
-        if(num % numbers[i]==0){
+    for(int i=1;i<num;i++){
+        if(num % i==0){
             c = c+1;}
     }
-    if(c== 1){
+    if(num!=1 && c== 1){
         cout<<"The num is prime"<<endl;
     }
     else{
-        cout<<"The num is non-proime number: ";
+        cout<<"The num is non prime number: "<<endl;
         for(int i=0;i<11;i++){
             cout<<numbers[i]<<endl;
     }}

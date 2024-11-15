0799c32daef643861e168ddb2bb40cea32ef85d0
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-11-15 09:28:06 +0000

    Testing Fri Nov 15 2024 09:28:06 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex22/student/main.cpp
+++ chapter4/ex22/student/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include<iomanip>
+
 
 using namespace std;
 
 int main() {
-    // Write your main here
+    int num;
+    int numbers[] = {2,3,5,7,9,11,13,17,19,23,29,31};
+    cout<<"Enter a num betwen 1-1000: "<<endl;
+    cin>>num;
+    int c = 0;
+    for(int i=1;i<11;i++){
+        if(num % numbers[i]==0)
+            c = c+1;
+    }
+    if(c== 1)
+        cout<<"The num is prime"<<endl;
+    else{
+        cout<<"The num is non-proime number: ";
+        for(int i=1;i<11;i++){
+            cout<<numbers[i];
+    }}
+
     return 0;
 }

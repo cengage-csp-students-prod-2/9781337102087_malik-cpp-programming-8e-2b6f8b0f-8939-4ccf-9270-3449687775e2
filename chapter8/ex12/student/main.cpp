a7ebeb3c666276688f14a27255285d2a8ca13b98
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-11-29 09:53:33 +0000

    Testing Fri Nov 29 2024 09:53:33 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex12/student/main.cpp
+++ chapter8/ex12/student/main.cpp
@@ -5,12 +5,82 @@
 
 using namespace std;
 
-void getData(ifstream& inf, string n[], double runData[][8], int count);
-void calculateAverage(double runData[][8], int count);
-void print(string n[], double runData[][8], int count);
+const int NUM_DAYS = 7;
+const int RUNNERS = 5;
+
+
+void getData(fstream& inf, string n[], double runData[][NUM_DAYS], int count){
+
+for ( int i =0 ; i< NUM_DAYS ; i++){
+    inf >> n[i];
+    for (int j =0 ; j< NUM_DAYS; i++){
+        inf >> runData[i][j];
+    }
+}
+       
+      
+
+     
+
+
+}
+void calculateAverage(double runData[][NUM_DAYS], double RUNNERS , double dailyAverage[]){
+    for (int days = 0; days< NUM_DAYS; days++){
+        double sum = 0.0;
+        for (int j = 0 ; j<RUNNERS;j++){
+            sum += runData[j][days];
+        }
+        dailyAverage[days] = sum/RUNNERS;
+    }
+}
+
+
+
+void print(string n[], double runData[][NUM_DAYS],  double RUNNERS , double dailyAverage[]){
+    for (int i =0; i< RUNNERS;i++){
+        cout<<"Runners Name "<< i+1 <<" : "<< n[i];
+        for (int j =0 ; j< NUM_DAYS ; j++){
+            cout<<"Running data: "<< runData[i][j];
+        }
+        cout<<endl;
+    }
+
+    // printing daily average
+    for (int j =0 ; j< NUM_DAYS ; j++){
+        cout<<"Daily Average data: "<< dailyAverage[j]<<endl;
+        }
+        
+
+}
+
+
+
   
 int main()
 {
-    // Write your main here
+    string names[RUNNERS];
+    double runningData[RUNNERS][NUM_DAYS];
+    double dailyAverage[NUM_DAYS];
+    
+
+    fstream inFile;
+    
+    inFile.open("ch8_Ex12Data.txt");
+    if (!inFile){
+        cerr<<"File can't opened."<<endl;
+        return 1;
+    }
+    
+  
+    getData(inFile, names, runningData, RUNNERS);
+
+    calculateAverage(runningData , RUNNERS, dailyAverage);
+
+    print(names, runningData, RUNNERS, dailyAverage);
+    
+    
+    inFile.close();
+
+    
 	return 0;
 }

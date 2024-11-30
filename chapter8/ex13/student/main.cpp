0514ebaa24ed81b9170d07e6d1e684d54b873124
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-11-30 08:22:45 +0000

    Testing Sat Nov 30 2024 08:22:45 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex13/student/main.cpp
+++ chapter8/ex13/student/main.cpp
@@ -1,134 +1,90 @@
 #include <iostream>
-#include<iomanip>
-#include<string>
-#include<fstream>
-
+#include <iomanip>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
 const int NumStds = 10;
 const int NumSub = 5;
 
-void getData(fstream& inf, string n[], double mark[][NumSub] ){
-
-for (int i = 0 ; i< NumStds; i++){
-    inf >> n[i];
-    for (int j=0; j< NumSub; j++){
-        inf>> mark[i][j];
+void getData(fstream& inf, string n[], double mark[][NumSub]) {
+    for (int i = 0; i < NumStds; i++) {
+        inf >> n[i];
+        for (int j = 0; j < NumSub; j++) {
+            inf >> mark[i][j];
+        }
     }
 }
 
+void calculateAverageGrade(double testaverage[], string grade[], double mark[][NumSub], double& classaverage) {
+    double totalSum = 0.0;
 
+    for (int nam = 0; nam < NumStds; nam++) {
+        double sum = 0.0;
+        for (int subj = 0; subj < NumSub; subj++) {
+            sum += mark[nam][subj];
+        }
+        testaverage[nam] = sum / NumSub; // Average for each student
+        totalSum += testaverage[nam];    // Sum for class average
+
+        // Determine letter grade
+        if (testaverage[nam] >= 90) {
+            grade[nam] = "A";
+        } else if (testaverage[nam] >= 80) {
+            grade[nam] = "B";
+        } else if (testaverage[nam] >= 70) {
+            grade[nam] = "C";
+        } else if (testaverage[nam] >= 60) {
+            grade[nam] = "D";
+        } else {
+            grade[nam] = "F";
+        }
+    }
+
+    classaverage = totalSum / NumStds; // Class average
 }
 
-void calculateAverageGrade(double testaverage[], string grade[], double mark[][NumSub], double classaverage){
-double sum = 0.0;
-for (int subj =0 ; subj < NumSub; subj++){
-    
-    for ( int nam = 0; nam < NumStds; nam++ ){
-        sum += mark[subj][nam];
+void output(string n[], double mark[][NumSub], double testaverage[], string grade[], double classaverage) {
+    cout << fixed << showpoint << setprecision(2);
+
+    cout << "\nClass Average: " << classaverage << endl;
+
+    // Print header
+    cout << left << setw(12) << "Name";
+    for (int day = 1; day <= NumSub; day++) {
+        cout << setw(8) << ("Mark " + to_string(day));
     }
-    
-}
-testaverage[0] = sum/50;
+    cout << setw(10) << "Average" << "Grade" << endl;
 
-for (int nam = 0; nam < NumStds; nam++){
-    double sum = 0.0;
-    double per = 0.0;
-    for (int subj =0 ; subj < NumSub; subj++  ){
-        sum += mark[nam][subj];
+    // Print student data
+    for (int i = 0; i < NumStds; i++) {
+        cout << left << setw(12) << n[i]; // Student name
+        for (int j = 0; j < NumSub; j++) {
+            cout << setw(8) << mark[i][j]; // Marks
+        }
+        cout << setw(10) << testaverage[i] << grade[i] << endl;
     }
-    per  = sum/5;
-    if (per >=90 && per <= 100){
-        grade[nam] = "A";
-    }
-    
-    else if (per >=80 && per <= 89.99)
-    {
-       grade[nam] = "B";
-    }
-    else if (per >=70 && per <= 79.99)
-    {
-       grade[nam] = "C";
-    }
-    else if (per >=60 && per <= 69.99)
-    {
-       grade[nam] = "D";
-    }
-    else{
-        grade[nam] = "F";
-    }
-    
-
-
-    
-
-}
-}
-
-void output(string n[], double mark[][NumSub], double testaverage[], string grade[]){
-
-cout<<fixed<<showpoint<<setprecision(2);
-
-  // Print test averages
-   cout << "\nClass Average: ";
-//    cout << left << setw(12) << " ";
-//    for (int day = 0; day < NumSub; day++) {
-//        cout << setw(8) << testaverage[day];
-//    }
-    cout<< testaverage[0];
-   cout << endl;
-
-// Print header
-   cout << left << setw(12) << "Name";
-   for (int day = 1; day <= NumSub; day++) {
-       cout << setw(8) << ("Mark " + to_string(day));
-   }
-   cout << endl;
-
-
-   // Print student data
-   for (int i = 0; i < NumStds; i++) {
-       cout << left << setw(12) << n[i];  // student name
-       for (int j = 0; j < NumSub; j++) {
-           cout << setw(8) << mark[i][j];  // Mark for each day
-       }
-       cout << endl;
-   }
-
-
- 
-
-
-// Print grade 
-   cout << "\n Grade:" << endl;
-   cout << left << setw(12) << " ";
-   for (int day = 0; day < NumStds ; day++) {
-       cout << setw(8) << grade[day];
-   }
-   cout << endl;
-
-
 }
 
 int main() {
-    // Write your main here
     string names[NumStds];
     double marks[NumStds][NumSub];
     string grade[NumStds];
-    double testaverage[0];
+    double testaverage[NumStds];
     double classaverage;
 
     fstream inFile;
     inFile.open("ch8_Ex13Data.txt");
-    getData(inFile, names, marks );
+    if (!inFile) {
+        cerr << "Error opening file." << endl;
+        return 1;
+    }
 
+    getData(inFile, names, marks);
+    calculateAverageGrade(testaverage, grade, marks, classaverage);
+    output(names, marks, testaverage, grade, classaverage);
 
-    calculateAverageGrade( testaverage, grade, marks,classaverage);
-
-    output(names, marks, testaverage, grade);
-
-
-
+    inFile.close();
     return 0;
 }

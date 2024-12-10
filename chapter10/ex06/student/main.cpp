f1b21fb7dae19d1eadb4c8a62b4a733cdbd16878
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-12-10 01:20:46 +0000

    Testing Tue Dec 10 2024 01:20:45 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex06/student/main.cpp
+++ chapter10/ex06/student/main.cpp
@@ -1,8 +1,68 @@
 #include <iostream>
+#include <string>
+#include <map>
 
 using namespace std;
 
+class romanType {
+private:
+    string roman;
+    int integer;
+
+    // Map of Roman numerals and their corresponding integer values
+    map<char, int> romanToInt = {
+        {'M', 1000}, {'D', 500}, {'C', 100}, 
+        {'L', 50}, {'X', 10}, {'V', 5}, {'I', 1}
+    };
+
+public:
+    // Constructor
+    romanType(string r = "") {
+        roman = r;
+        integer = convertToInteger();
+    }
+
+    // Function to convert Roman numeral to integer
+    int convertToInteger() {
+        int total = 0;
+        int prevValue = 0;
+
+        // Traverse from the end of the Roman numeral
+        for (int i = roman.length() - 1; i >= 0; i--) {
+            int value = romanToInt[roman[i]];
+
+            if (value >= prevValue)
+                total += value;
+            else
+                total -= value;
+
+            prevValue = value;
+        }
+        return total;
+    }
+
+    // Function to print the Roman numeral
+    void printRoman() const {
+        cout << "Roman numeral: " << roman << endl;
+    }
+
+    // Function to print the integer value
+    void printInteger() const {
+        cout << "Integer value: " << integer << endl;
+    }
+};
+
+// Main function to test the program
 int main() {
-    // Write your main here
+    string testNumerals[] = {"MCXIV", "CCCLIX", "MDCLXVI"};
+
+    for (const auto& numeral : testNumerals) {
+        romanType romanNum(numeral);
+        romanNum.printRoman();
+        romanNum.printInteger();
+        cout << "-----------------------------" << endl;
+    }
+
     return 0;
 }
+

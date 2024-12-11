fa350e4b61fe2d673f0a02a6953609ed48ea47dc
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-12-11 22:51:55 +0000

    Testing Wed Dec 11 2024 22:51:55 GMT+0000 (Coordinated Universal Time)

--- chapter9/ex04/student/main.cpp
+++ chapter9/ex04/student/main.cpp
@@ -2,90 +2,96 @@
 #include <iomanip>
 #include <fstream>
 #include <string>
-#include <vector>
 using namespace std;
-
-struct MenuItem {
-    string name;
-    double price;
+const int NO_OF_ITEMS = 8;
+struct menuItemType {
+    string menuItem;
+    double menuPrice;
 };
-
-// Function to load menu data from file
-void getData(vector<MenuItem>& menuList) {
-    ifstream infile("Ch9_Ex4Data.txt");
-    if (!infile) {
-        cerr << "Error opening file!" << endl;
-        exit(1);
-    }
-
-    string itemName;
-    double itemPrice;
-    while (getline(infile, itemName) && infile >> itemPrice) {
-        infile.ignore();  // Ignore the newline after the price
-        menuList.push_back({itemName, itemPrice});
-    }
-    infile.close();
-}
-
-// Function to display the menu
-void showMenu(const vector<MenuItem>& menuList) {
-    cout << "Welcome to Johnny's Restaurant" << endl;
-    cout << "----Today's Menu----" << endl;
-
-    for (size_t i = 0; i < menuList.size(); ++i) {
-        cout << i + 1 << ": " << left << setw(15) << menuList[i].name
-             << "$" << fixed << setprecision(2) << menuList[i].price << endl;
-    }
-    cout << endl;
-}
-
-// Function to calculate and print the bill
-void printCheck(const vector<MenuItem>& menuList, const vector<int>& order) {
-    if (order.empty()) {
-        cout << "No items ordered." << endl;
-        return;
-    }
-
-    double total = 0.0;
-    cout << "Welcome to Johnny's Restaurant" << endl;
-    for (int index : order) {
-        cout << left << setw(15) << menuList[index].name
-             << "$" << fixed << setprecision(2) << menuList[index].price << endl;
-        total += menuList[index].price;
-    }
-
-    double tax = total * 0.05;
-    double amountDue = total + tax;
-
-    cout << left << setw(15) << "Tax" << "$" << fixed << setprecision(2) << tax << endl;
-    cout << left << setw(15) << "Amount Due" << "$" << fixed << setprecision(2) << amountDue << endl;
-}
+void getData(ifstream& inFile, menuItemType mList[], int listSize);
+void showMenu(menuItemType mList[], int listSize);
+bool isItemSelected(int cList[], int cListLength, int itemNo);
+void makeSelection(int listSize, int cList[], int& cListLength);
+void printCheck(menuItemType mList[], int listSize, int cList[], int cListLength);
 
 int main() {
-    vector<MenuItem> menuList;
-    getData(menuList);
-    showMenu(menuList);
-
-    vector<int> order;
-    int itemNumber;
-    char choice;
-
-    do {
-        cout << "Enter item number (1-8): ";
-        cin >> itemNumber;
-
-        if (itemNumber < 1 || itemNumber > 8) {
-            cout << "Invalid item number. Please try again." << endl;
-        } else {
-            order.push_back(itemNumber - 1);
-        }
-
-        cout << "Select another item Y/y (Yes), N/n (No): ";
-        cin >> choice;
-    } while (choice == 'Y' || choice == 'y');
-
-    cout << endl;
-    printCheck(menuList, order);
+    menuItemType menuList[NO_OF_ITEMS];  
+    int choiceList[NO_OF_ITEMS];        
+    int choiceListLength = 0;            
+    ifstream inFile;  
+        cout << fixed << showpoint << setprecision(2);
+    menuList[0] = {"Plain Egg", 1.45};
+    menuList[1] = {"Bacon and Egg", 2.45};
+    menuList[2] = {"Muffin", 0.99};
+    menuList[3] = {"French Toast", 1.99};
+    menuList[4] = {"Fruit Basket", 2.49};
+    menuList[5] = {"Cereal", 0.69};
+    menuList[6] = {"Coffee", 0.50};
+    menuList[7] = {"Tea", 0.75};
+    showMenu(menuList, NO_OF_ITEMS);
+    makeSelection(NO_OF_ITEMS, choiceList, choiceListLength);
+    printCheck(menuList, NO_OF_ITEMS, choiceList, choiceListLength);
 
     return 0;
+}
+void getData(ifstream& inFile, menuItemType mList[], int listSize) {
+    string item;
+    double price;
+        for (int i = 0; i < listSize; ++i) {
+        inFile >> item >> price;
+        mList[i].menuItem = item;
+        mList[i].menuPrice = price;
+    }
+}
+void showMenu(menuItemType mList[], int listSize) {
+    cout << "Welcome to Johnny's Restaurant" << endl;
+    cout << "----Today's Menu----" << endl;
+    for (int i = 0; i < listSize; ++i) {
+        cout << i + 1 << ": " << left << setw(15) << mList[i].menuItem << "$" << mList[i].menuPrice << endl;
+    }
+    cout << endl;
+    cout << "You can make up to 8 single order selections" << endl;
+}
+bool isItemSelected(int cList[], int cListLength, int itemNo) {
+    for (int i = 0; i < cListLength; ++i) {
+        if (cList[i] == itemNo) {
+            return true;
+        }
+    }
+    return false;
+}
+void makeSelection(int listSize, int cList[], int& cListLength) {
+    char selection;
+    int itemNo;
+    do {
+        cout << "Do you want to make selection Y/y (Yes), N/n (No): ";
+        cin >> selection;
+        if (selection == 'Y' || selection == 'y') {
+            cout << "Enter item number: ";
+            cin >> itemNo;
+            if (itemNo >= 1 && itemNo <= listSize) {
+                if (!isItemSelected(cList, cListLength, itemNo)) {
+                    cList[cListLength] = itemNo;
+                    cListLength++;
+                } else {
+                    cout << "You already selected this item!" << endl;
+                }
+            } else {
+                cout << "Invalid item number. Try again." << endl;
+            }
+        }
+    } while (selection == 'Y' || selection == 'y');
+}
+void printCheck(menuItemType mList[], int listSize, int cList[], int cListLength) {
+    double total = 0;
+    cout << "Welcome to Johnny's Restaurant" << endl;
+    for (int i = 0; i < cListLength; ++i) {
+        int itemNo = cList[i];
+        cout << left << setw(15) << mList[itemNo - 1].menuItem << "$" << mList[itemNo - 1].menuPrice << endl;
+        total += mList[itemNo - 1].menuPrice;
+    }
+    double tax = total * 0.05;
+    double amountDue = total + tax;
+    cout << "Tax             $" << tax << endl;
+    cout << "Amount Due      $" << amountDue << endl;
 }
